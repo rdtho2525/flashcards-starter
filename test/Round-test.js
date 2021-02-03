03bd94fbd19b06b7e7c9adadcf89d1b173f4ed34
Reggie Thompson <reggie.thompson4@gmail.com>
2021-02-03 15:14:03 -0700

    Finsih drafting test, write implementation code

--- test/Round-test.js
+++ test/Round-test.js
@@ -7,62 +7,67 @@ const Turn = require('../src/Turn');
 const Round = require('../src/Round');
 
 describe('Round', function() {
-    //should be a function
-    it.skip('should be a function', function() {
-        const round = new Round();
+
+    it('should be a function', function() {
+        const deck = new Deck();
+        const round = new Round(deck);
         expect(Round).to.be.a('function');
     });
 
-    //should hold a deck
-    it.skip('should be able to hold a deck', function() {
-        const deck = new Deck();
+    it('should be able to hold a deck', function() {
+        const card1 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+        const card2 = new Card(4, "What type of prototype method does not modify the existing array but returns a particular representation of the array?", ["mutator method", "accessor method", "iteration method"], "accessor method");
+        const card3 = new Card(6, "What is an example of a mutator method?", ["sort()", "map()", "join()"], "sort()");
+        const deck = new Deck([card1, card2, card3]);
         const round = new Round(deck);
-        expect(round.deck).to.deep.equal(deck);
+        expect(round.deck).to.equal(deck.currentDeck);
     });
 
-    //should start out with zero turns
-    it.skip('should start out with zero turns', function() {
+    it('should start out with zero turns', function() {
         const deck = new Deck();
         const round = new Round(deck);
         expect(round.turns).to.equal(0);
     });
 
-    //should start out with zero incorrect guesses
-    it.skip('should start out with zero incorrect guesses', function() {
+    it('should start out with zero incorrect guesses', function() {
         const deck = new Deck();
         const round = new Round(deck);
         expect(round.incorrectGuesses.length).to.equal(0);
     })
 
-    //should return current card
-    it.skip('should return the first card in the deck', function() {
-        const deck = new Deck();
+    it('should return the first card in the deck', function() {
+        const card1 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+        const card2 = new Card(4, "What type of prototype method does not modify the existing array but returns a particular representation of the array?", ["mutator method", "accessor method", "iteration method"], "accessor method");
+        const deck = new Deck([card1, card2]);
         const round = new Round(deck);
         expect(round.returnCurrentCard).to.be.a('function');
-        expect(round.returnCurrentCard()).to.equal(/*card obj*/);
+        expect(round.returnCurrentCard()).to.equal(card1);
     });
 
-    //should allow player to take a turn guessing the answer
-    it.skip('should allow a player to take a turn guessing the answer', function() {
-        const deck = new Deck();
+    it('should allow a player to take a turn guessing the answer', function() {
+        const card1 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+        const card2 = new Card(4, "What type of prototype method does not modify the existing array but returns a particular representation of the array?", ["mutator method", "accessor method", "iteration method"], "accessor method");
+        const card3 = new Card(6, "What is an example of a mutator method?", ["sort()", "map()", "join()"], "sort()");
+        const deck = new Deck([card1, card2, card3]);
         const round = new Round(deck);
-        expect(round.takeTurn('object')).to.equal('correct!');
+        expect(round.takeTurn('array')).to.equal('correct!');
     });
 
-    //taking a turn should increment the turns property
-    it.skip('taking a turn should increment the turns property', function() {
-        const deck = new Deck();
+    it('taking a turn should increment the turns property', function() {
+        const card1 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"], "array");
+        const card3 = new Card(6, "What is an example of a mutator method?", ["sort()", "map()", "join()"], "sort()");
+        const deck = new Deck([card1, card3]);
         const round = new Round(deck);
-        round.takeTurn('object');
-        round.takeTurn('pup');
+        round.takeTurn('array');
+        round.takeTurn('sort()');
         expect(round.turns).to.equal(2);
     });
 
-    //should store the id of cards that were guessed incorrectly
-    it.skip('should store the id of cards that were guessed incorrectly', function() {
-        const deck = new Deck();
+    it('should store the id of cards that were guessed incorrectly', function() {
+        const card3 = new Card(6, "What is an example of a mutator method?", ["sort()", "map()", "join()"], "sort()");
+        const deck = new Deck([card3]);
         const round = new Round(deck);
-        round.takeTurn('pup');
-        expect(round.incorrectGuesses[0]).to.equal(/*card id*/)
+        round.takeTurn('map()');
+        expect(round.incorrectGuesses[0]).to.equal(6);
     });
 });
