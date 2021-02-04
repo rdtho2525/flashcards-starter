599455ba648eee600988d29fdce2479c9d8ce39b
Reggie Thompson <reggie.thompson4@gmail.com>
2021-02-04 08:23:33 -0700

    Add initial implementation code

--- src/Game.js
+++ src/Game.js
@@ -2,8 +2,22 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+const Card = require('../src/Card');
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
+
 class Game {
-  constructor() {}
+  constructor() {
+    this.newDeck = new Deck([]);
+    this.currentRound = null;
+  }
+
+  createCards() {
+    prototypeQuestions.forEach(ele => {
+      const newCard = new Card(ele.id, ele.question, ele.answers, ele.correctAnswer);
+      this.newDeck.cards.push(newCard);
+    });
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -11,7 +25,15 @@ class Game {
   }
 
   printQuestion(round) {
-      util.main(round);
+    util.main(round);
+  }
+
+  start() {
+    this.createCards();
+    this.currentRound = new Round(this.newDeck);
+    console.log(this.newDeck.length)
+    this.printMessage(this.newDeck, this.currentRound);
+    this.printQuestion(this.currentRound);
   }
 }
 
