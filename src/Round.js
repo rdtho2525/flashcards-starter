f829373c84ed4dbfb0ef4a19cdb5d46b6456de92
Reggie Thompson <reggie.thompson4@gmail.com>
2021-02-04 16:20:12 -0700

    Fix returnCurrentCard output, move shift() call to takeTurn

--- src/Round.js
+++ src/Round.js
@@ -2,21 +2,22 @@ const Turn = require('../src/Turn');
 
 class Round {
     constructor(deck) {
-        this.deck = deck.currentDeck;
+        this.deck = deck.cards;
         this.turns = 0;
         this.incorrectGuesses = [];
     }
 
     returnCurrentCard() {
-        return this.deck.shift();
+        return this.deck[0];
     }
 
     takeTurn(guess) {
-        this.turns++
+        this.turns++;
         const currentCard = this.returnCurrentCard();
         const turn = new Turn(guess, currentCard);
         const feedback = turn.giveFeedback();
         feedback === 'incorrect!' ? this.incorrectGuesses.push(currentCard.id) : null;
+        this.deck.shift();
         return feedback
     }
 
@@ -27,9 +28,7 @@ class Round {
     }
 
     endRound() {
-        if (!this.deck.length) {
-            console.log(`** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`)
-        }
+        console.log(`** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`)
     }
 }
 
