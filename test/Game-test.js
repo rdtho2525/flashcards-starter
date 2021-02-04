4152159d29d7a0a1eed62eb6ea14cdc419f6bd93
Reggie Thompson <reggie.thompson4@gmail.com>
2021-02-04 16:54:00 -0700

    Add beforeEach statements to each test suite

--- test/Game-test.js
+++ test/Game-test.js
@@ -7,32 +7,33 @@ const Round = require('../src/Round');
 const Game = require('../src/Game');
 
 describe('Game', function() {
+    let card1;
+    let game;
+
+    beforeEach(function() {
+        card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+        game = new Game();
+    });
     
     it('should be a function', function() {
-        const game = new Game();
         expect(Game).to.be.a('function');
     });
 
     it('should start the game', function() {
-        const game = new Game();
         expect(game.start).to.be.a('function');
     });
 
     it('should be able to create cards', function() {
-        const game = new Game();
         game.start();
         expect(game.newDeck.cards[0]).to.be.instanceOf(Card)
     });
 
     it('should be able to put cards in a deck', function() {
-        const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-        const game = new Game();
         game.start();
         expect(game.newDeck.cards).to.deep.include(card1)
     });
 
     it('should be able to create a new round using the current deck', function() {
-        const game = new Game();
         game.start();
         expect(game.currentRound).to.be.instanceOf(Round);
         expect(game.newDeck).to.be.instanceOf(Deck);
